a2f19fc2ab1b7eabb85eb70c51ef22024ff64b86
taranipawan <taranipawan7@gmail.com>
2022-07-27 23:03:05 +0530

    First Commit

---
+++ app/src/main/java/com/satnam/ksm/sukhmani_sahib.java
@@ -0,0 +1,14 @@
+package com.satnam.ksm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sukhmani_sahib extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sukhmani_sahib);
+    }
+}
